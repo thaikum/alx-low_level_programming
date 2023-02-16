f238520c129ffb886b6fc28981ec82020879fe05
thaikum <fredthaiku@gmail.com>
2023-02-16 14:00:26 -0800

    advanced

---
+++ 0x00-hello_world/quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - prints to the standard error
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
