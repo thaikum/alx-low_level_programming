bc52bdb12d1bff58219164ce8b649140f3c4fe9f
thaikum <fredthaiku@gmail.com>
2023-02-21 12:51:45 -0800

    adder

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: second number
+ *
+ * Return: a + b
+ */
+int add(int a + int b)
+{
+	int sum = a + b;
+	return (sum);
+}
