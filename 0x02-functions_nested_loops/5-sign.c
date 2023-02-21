72722f76c83f092c86bef57b425ef2e3c5f08a95
thaikum <fredthaiku@gmail.com>
2023-02-21 05:47:32 -0800

    five

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_sign - this function determines whether a number is
+ * or 0, positive or negative
+ * @n: the number to be checked;
+ *
+ * Return: + if positive, - if negative and 0 if 0
+ */
+int print_sign(int n)
+{
+	return (n > 0 ? '+' : n < 0 ? '-' : 0);
+}
