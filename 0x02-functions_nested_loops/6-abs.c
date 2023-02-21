00afc99ff0f2e327d1c5237da7018353cfbfdedf
thaikum <fredthaiku@gmail.com>
2023-02-21 06:15:02 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _abs - return the absolute of a number
+ *@n: the number to be checked
+ *
+ * Return: the absolute of number
+ */
+int _abs(int n)
+{
+	return (n < 0 ? n * -1 : n);
+}
