7c87775db925a2be34a41632903ee6e1c44a69b3
thaikum <fredthaiku@gmail.com>
2023-02-21 12:33:25 -0800

    fix

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * times_table - prints the multiplication table
+ *
+ */
+void times_table(void);
+{
+	int _1;
+	int _2;
+	int prod;
+
+	for (_1 = 0; _1 < 10; _1++)
+	{
+		for (_2 = 0; _2 < 10; _2++)
+		{
+			prod = _1 * _2;
+
+			if (prod >= 10)
+				_putchar('0' + prod / 10);
+			_putchar('0' + prod % 10);
+			_putchar(',')
+			_putchar('\t');
+		}
+		_putchar('\n');
+	}
+}
