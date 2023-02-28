8c159db824e0c69aac8e1a326357f2c4ea7282b4
thaikum <fredthaiku@gmail.com>
2023-02-28 12:29:39 -0800

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,13 +3,13 @@
 
 /**
  * print_rev - prints a string in reverse
- * @s: string to print
+ * @str: string to print
  */
 void print_rev(char *str)
 {
 	int x = strlen(str) - 1;
 
-	while(x >= 0)
+	while (x >= 0)
 	{
 		_putchar(str[x]);
 		x--;
