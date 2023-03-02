7f4269861ba906e75b4b3f7fe94f97038b2fe5a5
thaikum <fredthaiku@gmail.com>
2023-03-01 19:57:12 -0800

    compare

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@ char *_strcat(char *dest, char *src);
 void _putchar(int);
 void *_strncat(char *dest, char *src, int n);
 void *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 #endinf
