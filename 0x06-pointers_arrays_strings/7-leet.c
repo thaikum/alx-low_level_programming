50623dff08f588288d3ff929430f1339c569c282
thaikum <fredthaiku@gmail.com>
2023-03-02 01:28:07 -0800

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+/**
+ * leet - replaces aeotl with 43071
+ * @src: string to replace
+ *
+ * Return: the replaces string
+ */
+char *leet(char *src)
+{
+	char str1[] = "aeioutlAEIOUTL";
+	char str2[] = "4307143071";
+
+	int x, y;
+
+	for (x = 0; src[x] != '\0'; x++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			if (src[x] == str1[y])
+			{
+				src[x] = str2[y];
+				break;
+			}
+		}
+	}
+	return (src);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,4 +9,5 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 int is_special(char);
+char *leet(char *);
 #endif
