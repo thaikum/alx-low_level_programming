ea4da7e518abd9734ed41ca4679388692a46b2f0
thaikum <fredthaiku@gmail.com>
2023-03-07 04:36:17 -0800

    fifth

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include <stddef.h>
+#include <strin.h>
+/**
+ * _strstr - finds the first occurrence of the substring needle
+ * in the string haystack.
+ * @haystack: the parent string
+ * @needle: the string to check
+ *
+ * Return: a pointer ot the first  index of the substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int k = 0;
+	char *t = strchr(haystack, needle[0]);
+
+	if (t != NULL)
+		for (; needle[k] != '\0'; k++)
+			if (needle[k] != *(t + k))
+				haystack = t + 1;
+
+	if (k == strlen(needle))
+		return (t);
+	else
+		return (NULL);
+}
