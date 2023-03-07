e98b92b21631fbcedf66dee5c19b6b286bd07373
thaikum <fredthaiku@gmail.com>
2023-03-06 20:47:21 -0800

    strspn

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -4,4 +4,5 @@ void _putchar(int);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, chr *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
 #endif
