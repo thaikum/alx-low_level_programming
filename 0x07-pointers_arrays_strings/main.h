699a8d2e6f8489f32458e9e61e3ced98baa5e372
thaikum <fredthaiku@gmail.com>
2023-03-07 02:54:03 -0800

    fix error in main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_HEADER
 void _putchar(int);
 char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, chr *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 #endif
