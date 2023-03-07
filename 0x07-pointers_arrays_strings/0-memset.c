23e801416fbb44b72f1337c71a7d191f24224f86
thaikum <fredthaiku@gmail.com>
2023-03-06 19:42:25 -0800

    first

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+/**
+ * _memset - fills the first n bytes of memofy area pointed to by s
+ * with a constant byte b
+ * @s: the location to copy to
+ * @b: character to copy
+ * @n: number of times
+ *
+ * Return: pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int k = 0;
+
+	for (; k < n; k++)
+	{
+		s[k] = b;
+	}
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+void _putchar(int);
+char *_memset(char *s, char b, unsigned int n);
+#endif
