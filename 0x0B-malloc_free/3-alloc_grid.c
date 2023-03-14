3a9b4a6b6c593da6a704bc6f5e3ac44062f92357
thaikum <fredthaiku@gmail.com>
2023-03-14 06:06:09 -0700

    fix error

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	if (width == 0 && height == 0)
 		return (NULL);
 
-	dim = (int **)malloc(height * sizeof(int));
+	dim = (int **)malloc(height * width * sizeof(int));
 	if (dim == NULL)
 		return (NULL);
 
