d5bd795ecd0285c209d5d1fa4ade1c70ab44cb50
thaikum <fredthaiku@gmail.com>
2023-03-15 06:09:05 -0700

    fix

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the program
+ * @argc: size of argv
+ * @argv: an array
+ *
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
