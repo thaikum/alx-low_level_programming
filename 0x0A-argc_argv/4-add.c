e2ab10af06b3a8acd23c21f8369de977328d4f68
thaikum <fredthaiku@gmail.com>
2023-03-15 07:05:01 -0700

    fix again

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,36 +3,45 @@
 #include <string.h>
 
 /**
+ * invalid - checks whether a string contains an invalid character
+ * @str: the string to check
+ *
+ * Return: 1 if string is invalid and 0 if not
+ */
+int invalid(char *str)
+{
+	int x = 0;
+
+	for (; str[x] != '\0'; x++)
+		if (str[x] < '0' || str[x] > '9')
+			return (1);
+	return (0);
+}
+
+/**
  * main - prints the sum of the arguments
  * @argc: number of arguments
  * @argv: actual arguments
  *
  * Return: 0 if successful or 1 if not
  */
+
 int main(int argc, char *argv[])
 {
 	int x, sum = 0, num;
 
-	if (argc <= 1)
-		printf("0\n");
-	else
+	for (x = 1; x < argc; x++)
 	{
-		for (x = 1; x < argc; x++)
+		if (invalid(argv[x]))
 		{
-			num = atoi(argv[x]);
-
-			if (num == 0 && strcmp(argv[x], "0") != 0)
-			{
-				printf("Error\n");
-				return (1);
-			}
-			else
-			{
-				sum += num;
-			}
+			printf("Error\n");
+			return (1);
 		}
-		printf("%d\n", sum);
+
+		num = atoi(argv[x]);
+		sum += num;
 	}
+	printf("%d\n", sum);
 
 	return (0);
 }
