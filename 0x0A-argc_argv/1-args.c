ac518ee444b249962e4ace5d76474ee8ef75f1f7
thaikum <fredthaiku@gmail.com>
2023-03-15 06:21:38 -0700

    2

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,8 +7,8 @@
  *
  * Return: 0 always
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%s\n", argv[0]);
+	printf("%d\n", argc - 1);
 	return (0);
 }
