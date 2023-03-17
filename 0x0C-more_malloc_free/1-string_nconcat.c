b3c046385e1c9931c6324dd31c17f35d2a5d7eba
thaikum <fredthaiku@gmail.com>
2023-03-17 11:37:01 -0700

    adding a NULL checker

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,7 +13,15 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *k;
-	unsigned int len1 = strlen(s1), len2 = strlen(s2);
+	unsigned int len1, len2;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len1 = strlen(s1);
+	len2 = strlen(s2);
 
 	len2 = len2 <= n ? len2 : n;
 	k = malloc((len1 + len2 + 1) * sizeof(char));
