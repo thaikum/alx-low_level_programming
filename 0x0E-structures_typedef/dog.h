7503308164ed821dee183a1dd177581e58ba44b4
thaikum <fredthaiku@gmail.com>
2023-03-20 06:20:16 -0700

    first struct dog_t

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - dogs attributes
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: dogs attribute
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+#endif;
