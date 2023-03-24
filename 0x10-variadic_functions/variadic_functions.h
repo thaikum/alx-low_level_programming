30ab575e676939e277c79dbf9297002b9476ea23
thaikum <fredthaiku@gmail.com>
2023-03-24 00:40:10 -0700

    fix #endif in header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,4 +5,4 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char *const format, ...);
 void _putchar(int);
-#endinf
+#endif
