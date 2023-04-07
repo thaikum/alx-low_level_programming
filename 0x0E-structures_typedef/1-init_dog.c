551b7193ef3c5e3c55ba722bdf3d34b4717e46e3
thaikum <fredthaiku@gmail.com>
2023-04-07 03:06:55 -0700

    fixing null safety

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 /**
  * init_dog - initialized a dog struct
  * @d: pointer to the dog
@@ -8,6 +9,9 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
