af7fb11d33a10b893486a26a72286e5ad63e7d92
thaikum <fredthaiku@gmail.com>
2023-04-07 07:47:28 -0700

    fixing unused variables in main

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -11,19 +11,14 @@
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
 	int (*f)(int a, int b);
 
 	if (argc != 4)
 		exit(98);
-	
 	f = get_op_func(argv[2]);
 
 	if (f != NULL)
-	{
-		puts("the function is not null \n");
 		printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
-	}
 
 	return (0);
 }
