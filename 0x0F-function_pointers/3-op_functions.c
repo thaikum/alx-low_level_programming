0a70e0b7ec02257934ce43f2170fb765f72371de
thaikum <fredthaiku@gmail.com>
2023-04-07 08:11:55 -0700

    fixing error

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -43,7 +43,7 @@ int op_div(int a, int b)
 {
 	if (b == 0)
 	{
-		puts("Error\n");
+		puts("Error");
 		exit(100);
 	}
 	return (a / b);
@@ -59,7 +59,7 @@ int op_mod(int a, int b)
 {
 	if (b == 0)
 	{
-		puts("Error\n");
+		puts("Error");
 		exit(100);
 	}
 	return (a % b);
