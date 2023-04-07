31b88f7d244971e1d5e2964a147268bd38f4c509
thaikum <fredthaiku@gmail.com>
2023-04-07 07:53:26 -0700

    fixing error handling

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,8 +4,8 @@
 
 /**
  * main - simple calculator
- * @argc - number of arguments
- * @argv - arguments
+ * @argc: number of arguments
+ * @argv: arguments
  *
  * Return: 0 always
  */
@@ -13,12 +13,18 @@ int main(int argc, char *argv[])
 {
 	int (*f)(int a, int b);
 
-	if (argc != 4)
+	if (argc != 4){
+		printf("Error\n");
 		exit(98);
+	}
 	f = get_op_func(argv[2]);
 
 	if (f != NULL)
+	{
 		printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+		return (0);
+	}
 
-	return (0);
+	printf("Error\n");
+	exit(98);
 }
