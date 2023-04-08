5eb37b65f57f25046a68356d8b05d19ab2944259
thaikum <fredthaiku@gmail.com>
2023-04-08 07:17:17 -0700

    add element into a list

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * insert_at_index - inserts a node at a specific index
+ * @head: the first element of the list
+ * @idx: the index to insert at;
+ * @n: the value to insert
+ *
+ * Return: the newly created node
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int k = 0;
+	listint_t *current_node = *head, *swap_space;
+
+	while(current_node)
+	{
+		if (idx - k == 1)
+		{
+			swap_space = malloc(sizeof(listint_t));
+			if (!swap_space)
+				return (NULL);
+
+			swap_space->n = n;
+			swap_space->next = current_node->next;
+			current_node->next = swap_space;
+			return (swap_space);
+		}
+		k++;
+		current_node = current_node->next;
+	}
+	return (NULL);
+}
