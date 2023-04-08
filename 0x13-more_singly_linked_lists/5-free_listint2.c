71fac940390bbf5465e462391ffea753b2afce6a
thaikum <fredthaiku@gmail.com>
2023-04-08 05:22:14 -0700

    freeing list and its head

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - frees memory
+ * @head: the first element of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *cur_ptr = NULL;
+
+	if ((*head) == NULL)
+		return;
+
+	while ((*head)->next)
+	{
+		cur_ptr = (*head)->next;
+		free(*head);
+		*head = cur_ptr;
+	}
+	free(*head);
+	*head = NULL;
+}
