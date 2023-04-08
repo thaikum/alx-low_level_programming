f2ad77be71d1f7bd51370f0d23a5c669766eeb09
thaikum <fredthaiku@gmail.com>
2023-04-08 07:24:51 -0700

    fixing first index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,21 +15,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int k = 0;
 	listint_t *current_node = *head, *swap_space;
 
-	while(current_node)
+	do
 	{
-		if (idx - k == 1)
+		if (idx - k == 1 || idx = 0)
 		{
 			swap_space = malloc(sizeof(listint_t));
 			if (!swap_space)
 				return (NULL);
 
 			swap_space->n = n;
-			swap_space->next = current_node->next;
+			swap_space->next = current_node ?
+				current_node->next : NULL;
+			
 			current_node->next = swap_space;
 			return (swap_space);
 		}
 		k++;
 		current_node = current_node->next;
-	}
+	} while (current_node);
 	return (NULL);
 }
