f40fb114629d92d3b5c227976c17beaa30950184
thaikum <fredthaiku@gmail.com>
2023-04-10 08:49:45 -0700

    fixing betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -30,7 +30,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		if (s == errors[x])
 			return (0);
 
-	if ( s == write(STDOUT_FILENO, buff, letters))
+	if (s == write(STDOUT_FILENO, buff, letters))
 	{
 		free(buff);
 		buff = NULL;
