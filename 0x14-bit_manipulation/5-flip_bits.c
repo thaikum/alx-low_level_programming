0fca9c4f32820af10470917ab906134e777bd244
thaikum <fredthaiku@gmail.com>
2023-04-10 02:29:01 -0700

    flipping bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,27 @@
+/**
+ * count1 - counts number of 1 in a number binary
+ * @n: number to count set bits
+ */
+int count1(unsigned long int n)
+{
+	int x = 0;
+	while (n > 0) {
+		x++;
+		n &= (n - 1);
+	}
+	return x;
+}
+
+/**
+ * flip_bits - count numbers that need to be fliped
+ * @n: first number
+ * @m: desired Number
+ *
+ * Return: bits fliped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int v = n ^ m;
+	return (count1(v));
+
+}
