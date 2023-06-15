55c1dfc42905d7d6fa3d50e112da133d96e438bb
thaikum <fredthaiku@gmail.com>
2023-06-15 12:41:48 -0700

    fix return

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -13,7 +13,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
 	if (!new_node)
-		exit(98);
+		return (NULL);
 	if ((*head) != NULL)
 		(*head)->prev = new_node;
 
