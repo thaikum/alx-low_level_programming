907ba7d62b9a12af3c7ffc282a4ac4f36e853518
thaikum <fredthaiku@gmail.com>
2023-06-15 13:01:51 -0700

    task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_dlistint - frees a list
+ * @head: the list head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *h;
+
+	while(head)
+	{
+		h = head;
+		head = head->next;
+		free(h);
+	}
+}
