dece1e4bcb7cfd8de04872efb29c108e654db59b
thaikum <fredthaiku@gmail.com>
2023-06-15 12:18:41 -0700

    fix betty

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -5,12 +5,13 @@
  * print_dlistint - prints all elements of the list
  * @h: list head
  *
- * @Return: the size of the list
+ * Return: the size of the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
 	size_t x = 0;
-	while(h)
+
+	while (h)
 	{
 		printf("%d\n", h->n);
 		x++;
